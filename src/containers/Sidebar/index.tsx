be99f8b63faca47204ce13c899a78b64e578452e
Gabriel Pinto <gabrielsnaker@gmail.com>
2023-06-26 15:37:26 -0300

    construindo a estrutura e a seção "sobre mim"

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/Gabriel-OliveiraPinto.png"></img>
+    <Titulo fontSize={20}>Gabriel Pinto</Titulo>
+  </aside>
+)
+
+export default Sidebar
